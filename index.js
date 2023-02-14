d5af832d831a448deb8f135c8a2bce41d61689a0
fabianairc <fabianaramirezzz@gmail.com>
2023-02-14 09:41:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const nombre = "Fabiana";
+const apellido = "Ramirez Cuenca";
+
+const estudiante = nombre + " " + apellido;
+
+const estudianteMayus = estudiante.toUpperCase;
+const estudianteMinus = estudiante.toLowerCase;
+
+const conteo = estudiante.length;
+
+const primerLetra = nombre[0];
+const ultimaLetra = apellido[apellido.length - 1];
+
+if (estudiante.includes(nombre)) {
+  true;
+} else {
+  false;
+}
